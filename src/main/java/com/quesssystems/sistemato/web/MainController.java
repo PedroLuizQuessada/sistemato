45faf5513d568f2b3b02cbbcb43b362333273d86
Pedro Quessada <pedroluiz.quessada@gmail.com>
2022-10-14 10:08:50 -0300

    Rota para recuperar dados

--- src/main/java/com/quesssystems/sistemato/web/MainController.java
+++ src/main/java/com/quesssystems/sistemato/web/MainController.java
@@ -1,5 +1,7 @@
 package com.quesssystems.sistemato.web;
 
+import com.quesssystems.sistemato.beans.automacao.Automacao;
+import com.quesssystems.sistemato.beans.automacao.AutomacaoRepository;
 import com.quesssystems.sistemato.beans.usuario.Usuario;
 import com.quesssystems.sistemato.beans.usuario.UsuarioRepository;
 import com.quesssystems.sistemato.util.EmailUtil;
@@ -11,18 +13,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class MainController {
-    private final UsuarioRepository repo;
+    private final UsuarioRepository usuarioRepository;
+    private final AutomacaoRepository automacaoRepository;
     private final EmailUtil emailUtil;
 
-    public MainController(UsuarioRepository repo, EmailUtil emailUtil) {
-        this.repo = repo;
+    public MainController(UsuarioRepository usuarioRepository, AutomacaoRepository automacaoRepository, EmailUtil emailUtil) {
+        this.usuarioRepository = usuarioRepository;
+        this.automacaoRepository = automacaoRepository;
         this.emailUtil = emailUtil;
     }
 
@@ -46,12 +54,12 @@ public class MainController {
         String email = request.getParameter("email");
         String novaSenha = SenhaUtil.gerarNovaSenha();
 
-        Usuario usuario = repo.findByEmail(email);
+        Usuario usuario = usuarioRepository.findByEmail(email);
         if (usuario == null) {
             return "redirect:/login?emailinvalido";
         }
         usuario.setSenha(SenhaUtil.criptografar(novaSenha));
-        repo.save(usuario);
+        usuarioRepository.save(usuario);
 
         emailUtil.enviarEmail(email, "Nova senha para acesso ao Sistemato", String.format("<p>Sua senha para acesso ao Sistemato foi atualizada: <b>%s</b></p>", novaSenha));
 
@@ -71,5 +79,32 @@ public class MainController {
             default:
                 return "redirect:/automacoes/true";
         }
+    }
+
+    @GetMapping("/recuperardados/{idAutomacao}")
+    @ResponseBody
+    public Map<String, String> recuperarDados(@PathVariable("idAutomacao") Integer idAutomacao) {
+        Map<String, String> dados = new HashMap<>();
+
+        Optional<Automacao> optionalAutomacao = automacaoRepository.findById(idAutomacao);
+        if (optionalAutomacao.isPresent()) {
+            Automacao automacao = optionalAutomacao.get();
+            dados.put("status", String.format("Automação %d encontrada", idAutomacao));
+            dados.put("ativo", String.valueOf(automacao.isAtivo()));
+            dados.put("domingo", String.valueOf(automacao.isDomingo()));
+            dados.put("segunda", String.valueOf(automacao.isSegunda()));
+            dados.put("terca", String.valueOf(automacao.isTerca()));
+            dados.put("quarta", String.valueOf(automacao.isQuarta()));
+            dados.put("quinta", String.valueOf(automacao.isQuinta()));
+            dados.put("sexta", String.valueOf(automacao.isSexta()));
+            dados.put("sabado", String.valueOf(automacao.isSabado()));
+            dados.put("horario-inicio", automacao.getHorarioInicio());
+            dados.put("horario-fim", automacao.getHorarioFim());
+        }
+        else {
+            dados.put("status", String.format("Automação %d não encontrada", idAutomacao));
+        }
+
+        return dados;
     }
 }
