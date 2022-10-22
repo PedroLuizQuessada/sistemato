2b7c8fc92109501441852c7c9038b6b73a44b521
PedroLuizQuessada <pedroquessada@grupojuritas.com.br>
2022-10-22 15:45:41 -0300

    Dump da DB

---
+++ sistematodb.sql
@@ -0,0 +1,120 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sistemato
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `automacao`
+--
+
+use heroku_9cf4c25d5e5433a;
+
+DROP TABLE IF EXISTS `automacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8 */;
+CREATE TABLE `automacao` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ativo` bit(1) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `domingo` bit(1) NOT NULL,
+  `falha` varchar(255) DEFAULT NULL,
+  `horario_fim` varchar(255) DEFAULT NULL,
+  `horario_inicio` varchar(255) DEFAULT NULL,
+  `nome` varchar(45) NOT NULL,
+  `quarta` bit(1) NOT NULL,
+  `quinta` bit(1) NOT NULL,
+  `sabado` bit(1) NOT NULL,
+  `segunda` bit(1) NOT NULL,
+  `sexta` bit(1) NOT NULL,
+  `terca` bit(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_6ndfq6ol0xig16m6ny676ifiq` (`nome`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `automacao`
+--
+
+LOCK TABLES `automacao` WRITE;
+/*!40000 ALTER TABLE `automacao` DISABLE KEYS */;
+INSERT INTO `automacao` VALUES (1,_binary '','Envio de mensagens de texto e imagens pelo WhatsApp',_binary '\0',NULL,NULL,NULL,'Mensagens Whats',_binary '\0',_binary '\0',_binary '\0',_binary '\0',_binary '\0',_binary '\0');
+/*!40000 ALTER TABLE `automacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `execucao`
+--
+
+DROP TABLE IF EXISTS `execucao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8 */;
+CREATE TABLE `execucao` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `hora_execucao` datetime(6) NOT NULL,
+  `id_automacao` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKc1e31hseyl78gnq89tryqexyl` (`id_automacao`),
+  CONSTRAINT `FKc1e31hseyl78gnq89tryqexyl` FOREIGN KEY (`id_automacao`) REFERENCES `automacao` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `execucao`
+--
+
+LOCK TABLES `execucao` WRITE;
+/*!40000 ALTER TABLE `execucao` DISABLE KEYS */;
+INSERT INTO `execucao` VALUES (1,'2022-10-21 12:09:13.396000',1);
+/*!40000 ALTER TABLE `execucao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `adm` bit(1) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_kfsp0s1tflm1cwlj8idhqsad0` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,_binary '','pedroluiz.quessada@gmail.com','$2a$10$QYJSTqlh4jTrfqvyU.i2dOopv4VABH0oOct2y9Q.cyON6W62sOPj2'),(3,_binary '\0','moraes.jjoyce@gmail.com','$2a$10$/DXdLWpLIpoIrtAhzPSyfu5sddMdkWj9yF5kbdzjggEW9kqYJ0o1G');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-22  1:55:59
